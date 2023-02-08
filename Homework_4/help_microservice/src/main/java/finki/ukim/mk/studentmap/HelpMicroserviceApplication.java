5299496e29910979e41557edd161ca4a719662bd
Miss4Chan <despina.miseva@gmail.com>
2023-02-08 17:13:42 +0100

    microservices and eureka finished

--- Homework_4/help_microservice/src/main/java/finki/ukim/mk/studentmap/HelpMicroserviceApplication.java
+++ Homework_4/help_microservice/src/main/java/finki/ukim/mk/studentmap/HelpMicroserviceApplication.java
@@ -3,9 +3,9 @@ package finki.ukim.mk.studentmap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class HelpMicroserviceApplication {
     public static void main(String[] args) {
--- Homework_4/location_microservice/src/main/java/finki/ukim/mk/studentmap/LocationMicroserviceApplication.java
+++ Homework_4/location_microservice/src/main/java/finki/ukim/mk/studentmap/LocationMicroserviceApplication.java
@@ -2,12 +2,14 @@ package finki.ukim.mk.studentmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 //TODO add eureka client
+@EnableEurekaClient
 @SpringBootApplication
 public class LocationMicroserviceApplication {
     public static void main(String[] args) {
