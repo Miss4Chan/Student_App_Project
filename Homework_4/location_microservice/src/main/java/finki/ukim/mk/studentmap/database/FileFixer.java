c3b19b6cb6768d74ec32f4270fcfcee2a9003e04
Despina Misheva <despina.miseva@gmail.com>
2023-02-11 23:46:19 +0100

    Finished docker, run commands 'mvn clean install' 'docker-compose build --no-cache' and 'docker-compose up' for the images to be created, deployed, and started (access with localhost:PORTNUM)

--- Homework_4/location_microservice/src/main/java/finki/ukim/mk/studentmap/database/FileFixer.java
+++ Homework_4/location_microservice/src/main/java/finki/ukim/mk/studentmap/database/FileFixer.java
@@ -1,4 +1,17 @@
 package finki.ukim.mk.studentmap.database;
+import java.io.*;
 
-public class FileFixer {
+public class FileFixer{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/csvFiles/out.csv"));
+        PrintWriter pw = new PrintWriter(new FileWriter("src/main/java/com/example/studentmap/database/locations.csv"));
+
+        String line;
+        while (br.ready()){
+            line = br.readLine();
+            pw.write(line.replaceAll("\"", ""));
+            pw.write("\n");
+            pw.flush();
+        }
+    }
 }
